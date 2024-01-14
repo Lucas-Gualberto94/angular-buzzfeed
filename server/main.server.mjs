234cc824d10cc7bd8ea65bfce4309f11e26d56c2
Lucas Gualberto <lucas.gualberto.pereira@gmail.com>
2024-01-14 13:55:36 +0000

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-MMFZ6JSI.mjs";import"./chunk-P6X57EO7.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-SM3FC7WZ.mjs";import"./chunk-P6X57EO7.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
